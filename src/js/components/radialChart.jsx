ea0e23f6ebfd238a3e406caadcf165592e75529a
Boyer laurent <154669613+LaurentBoyer1976@users.noreply.github.com>
2025-04-01 15:56:39 +0200

    Ajout du composant RadialChart avec styles SCSS, mise à jour des styles de mise en page et ajout de gaps entre les colonnes.

--- src/js/components/radialChart.jsx
+++ src/js/components/radialChart.jsx
@@ -1,5 +1,6 @@
 import { RadialBarChart, RadialBar, ResponsiveContainer } from "recharts";
 import PropTypes from "prop-types";
+import "src/styles/components/radialChart.scss";
 
 /**
  * @description Composant RadialChart pour afficher un graphique radial.
@@ -22,52 +23,66 @@ const RadialChart = ({ data }) => {
   const endAngle = 90 + (360 * value) / 100; // Départ à 90° et progression selon le score
 
   return (
-    <ResponsiveContainer width="100%" height={300}>
-      <RadialBarChart
-        cx="50%"
-        cy="50%"
-        innerRadius="70%"
-        outerRadius="90%"
-        barSize={10}
-        data={data}
-        startAngle={90} // Départ à gauche de l'axe vertical
-        endAngle={endAngle} // Angle calculé en fonction du score
-      >
-        <RadialBar
-          minAngle={15}
-          clockWise
-          dataKey="value"
-          fill="#8884d8"
-          background={{ fill: "#eee" }}
-        />
-        {/* Texte au centre du graphique */}
-        <text
-          x="50%"
-          y="50%"
-          textAnchor="middle"
-          dominantBaseline="middle"
-          style={{ fontSize: "24px", fontWeight: "bold" }}
+    <div className="chartContainer__radialChart">
+      <ResponsiveContainer width="100%" height={300}>
+        <RadialBarChart
+          cx="50%"
+          cy="50%"
+          innerRadius="70%"
+          outerRadius="90%"
+          barSize={10}
+          data={data}
+          startAngle={90} // Départ à gauche de l'axe vertical
+          endAngle={endAngle} // Angle calculé en fonction du score
         >
-          {`${value}%`}
-        </text>
-        <text
-          x="50%"
-          y="60%"
-          textAnchor="middle"
-          dominantBaseline="middle"
-          style={{ fontSize: "16px", fill: "#555" }}
-        >
-          de votre objectif
-        </text>
-      </RadialBarChart>
-    </ResponsiveContainer>
+          {/* Titre "Score" en haut à gauche */}
+          <text
+            x="10%"
+            y="10%"
+            textAnchor="start"
+            dominantBaseline="middle"
+            style={{ fontSize: "15px", fontWeight: "500", fill: "#20253A" }}
+          >
+            Score
+          </text>
+
+          <RadialBar
+            minAngle={15}
+            clockWise
+            dataKey="value"
+            fill="#FF0000"
+            background={{ fill: "#FBFBFB" }}
+          />
+          {/* Texte au centre du graphique */}
+          <text
+            x="50%"
+            y="50%"
+            textAnchor="middle"
+            dominantBaseline="middle"
+            style={{ fontSize: "26px", fontWeight: "700", fill: "#282D30" }}
+          >
+            {`${value}%`}
+          </text>
+          <text
+            x="50%"
+            y="60%"
+            textAnchor="middle"
+            dominantBaseline="middle"
+            style={{ fontSize: "16px", fill: "#282D30" }}
+          >
+            de votre objectif
+          </text>
+        </RadialBarChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
+
 RadialChart.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.shape({
       value: PropTypes.number.isRequired,
-    }),
+    })
   ).isRequired,
 };
 
